d0dcae88df301c01c5820f0165ee0654cb5dab76
whosasking <105831892+RescueRapscallion@users.noreply.github.com>
2023-03-23 11:41:26 -0500

    first
    yep

---
+++ Assets/ClassExampels.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClassExampels : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        List<string> hello = new List<string>(1);
+        hello.Add("Wello");
+        hello.Add("World!");
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
